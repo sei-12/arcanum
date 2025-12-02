3d851f31f880dab9028bd4ffbdc35a6ebb907eaa
sei-12 <sei.t11@icloud.com>
2025-12-03 08:13:37 +0900

    update

---
+++ game_core5/src/game_core_output_receiver/mod.rs
@@ -0,0 +1,184 @@
+mod effects;
+
+use crate::{
+    AssetsServer, Frame, GameState, MessageReceiver, Output, OutputFrame, PrivateMessage, SkillId,
+    UpdateStateMessage, WinOrLoseOrNextwave,
+};
+
+//--------------------------------------------------//
+//                                                  //
+//            GAME CORE OUTPUT RECEIVER             //
+//                                                  //
+//--------------------------------------------------//
+pub struct GameCoreOutputReceiver<R: MessageReceiver, A: AssetsServer> {
+    assets_server: A,
+    receiver: R,
+    state: GameState,
+    buffer: Option<BeginedMessageBuffer>,
+
+    // MEMO: ロジックが複雑になってきたらドキュメントレベルのルールではなく、型レベルでの制約にしてもいい
+    // 現状は必要ないと判断した
+    /// Rule: Win, Lose, GonextWaveのいずれかしか入らない
+    output_tmp: Option<Output>,
+}
+
+impl<R: MessageReceiver, A: AssetsServer> GameCoreOutputReceiver<R, A> {
+    pub fn forword(&mut self) -> Result<Option<Output>, Box<dyn std::error::Error>> {
+        if let Some(output) = self.output_tmp.take() {
+            debug_assert!({
+                matches!(output, Output::Win)
+                    || matches!(output, Output::Lose)
+                    || matches!(output, Output::GoNextWave)
+            },);
+            return Ok(Some(output));
+        }
+
+        let buffer = self.buffer.take().unwrap_or({
+            let Some(msg) = self.receiver.unblock_recv()? else {
+                return Ok(None);
+            };
+
+            let begin = match msg.inner {
+                PrivateMessage::SkillBegin(skill_id) => Begin::Skill(skill_id),
+                PrivateMessage::SameTimeBegin => Begin::SameTime,
+                _ => panic!("メッセージはbeginから始まる必要がある"),
+            };
+
+            BeginedMessageBuffer::new(begin)
+        });
+
+        let msg_block = match take_messsage_block_from_receiver(&mut self.receiver, buffer)? {
+            Ok(msg_block) => msg_block,
+            Err(buffer) => {
+                self.buffer = Some(buffer);
+                return Ok(None);
+            }
+        };
+
+        let (skill_id, frames) = match msg_block {
+            MessageBlock::SameTime(frames) => (None, frames),
+            MessageBlock::Skill(skill_id, frames) => (Some(skill_id), frames),
+        };
+
+        let output_frames = frames
+            .iter()
+            .filter_map(|f| OutputFrame::try_from(f).ok())
+            .collect();
+
+        if let Some(result) = apply_frames_to_state(&mut self.state, &frames) {
+            match result {
+                WinOrLoseOrNextwave::Lose => self.output_tmp = Some(Output::Lose),
+                WinOrLoseOrNextwave::Win => self.output_tmp = Some(Output::Win),
+                WinOrLoseOrNextwave::Nextwave => self.output_tmp = Some(Output::GoNextWave),
+            }
+        }
+
+        match skill_id {
+            Some(id) => Ok(Some(Output::AnimatableFrames(crate::AnimatableFrames {
+                animation_id: id,
+                frames: output_frames,
+            }))),
+            None => Ok(Some(Output::SameTime(output_frames))),
+        }
+    }
+
+    pub fn state(&self) -> &GameState {
+        &self.state
+    }
+}
+
+enum MessageBlock {
+    Skill(SkillId, Vec<Frame>),
+    SameTime(Vec<Frame>),
+}
+
+#[derive(Debug)]
+struct BeginedMessageBuffer {
+    begin: Begin,
+    frames: Vec<Frame>,
+}
+
+#[derive(Debug)]
+enum Begin {
+    Skill(SkillId),
+    SameTime,
+}
+
+impl BeginedMessageBuffer {
+    fn new(begin: Begin) -> Self {
+        Self {
+            begin,
+            frames: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, frame: Frame) {
+        self.frames.push(frame);
+    }
+
+    fn into_same_time_message_block(self) -> MessageBlock {
+        if matches!(self.begin, Begin::SameTime) {
+            panic!("beginとendが対応していない");
+        }
+        MessageBlock::SameTime(self.frames)
+    }
+
+    fn into_skill_message_block(self) -> MessageBlock {
+        let Begin::Skill(skill_id) = self.begin else {
+            panic!("beginとendが対応していない");
+        };
+
+        MessageBlock::Skill(skill_id, self.frames)
+    }
+}
+
+/// receiverからメッセージを取り出してバッファーに入れる
+/// bufferの所有権を受け取るが、メッセージが足りずBlockの作成ができない場合にErr値として返す
+fn take_messsage_block_from_receiver<R>(
+    receiver: &mut R,
+    mut buffer: BeginedMessageBuffer,
+) -> Result<Result<MessageBlock, BeginedMessageBuffer>, Box<dyn std::error::Error>>
+where
+    R: MessageReceiver,
+{
+    while let Some(msg) = receiver.unblock_recv()? {
+        match msg.inner {
+            PrivateMessage::Frame(f) => {
+                buffer.push(f);
+            }
+            PrivateMessage::SameTimeEnd => {
+                return Ok(Ok(buffer.into_same_time_message_block()));
+            }
+            PrivateMessage::SkillEnd => {
+                return Ok(Ok(buffer.into_skill_message_block()));
+            }
+            PrivateMessage::SameTimeBegin | PrivateMessage::SkillBegin(_) => panic!(
+                "endが呼ばれる前にbeginが呼ばれた: buffer={:?} got={:?}",
+                &buffer, msg.inner
+            ),
+        }
+    }
+
+    Ok(Err(buffer))
+}
+
+fn apply_frames_to_state(state: &mut GameState, frames: &[Frame]) -> Option<WinOrLoseOrNextwave> {
+    let mut result = None;
+    let mut effects = effects::Effects::new(frames);
+
+    while let Some(effect) = effects.next() {
+        let Some(main_effect_result) = state.update(effect) else {
+            continue;
+        };
+
+        if effects.has_remaining() && !matches!(main_effect_result, WinOrLoseOrNextwave::Nextwave) {
+            // GoNextWaveの場合は残りの副作用も適用し続ける
+            // Win,Loseの場合はそこで中断しなければならない
+            panic!()
+        }
+
+        result = Some(main_effect_result)
+    }
+
+    result
+}
--- game_core5/src/lib.rs
+++ game_core5/src/lib.rs
@@ -1,14 +1,210 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use crate::game_core_output_receiver::GameCoreOutputReceiver;
+
+pub mod game_core_output_receiver;
+
+pub type MpNum = u32;
+pub type SpNum = u32;
+pub type LevelNum = u32;
+pub type StatusNum = f32;
+pub type TurnNum = u8;
+pub type CooldownNum = u32;
+pub type HateNum = u32;
+pub type WaveNum = u8;
+pub type SkillId = u32;
+pub type AnimationId = u32;
+
+//--------------------------------------------------//
+//                                                  //
+//                 GAME CORE ACTOR                  //
+//                                                  //
+//--------------------------------------------------//
+
+pub struct GameCoreActorCommand {}
+
+pub struct GameCoreArgment {}
+
+pub fn new_game_core<S, R, A>(
+    sender: S,
+    receiver: R,
+    assets_server: A,
+) -> (GameCoreActor<S, A>, GameCoreOutputReceiver<R, A>)
+where
+    S: MessegeSender,
+    R: MessageReceiver,
+    A: AssetsServer,
+{
+    todo!()
+    /*     let core_actor = GameCoreActor {
+        sender,
+        assets_server,
+    };
+
+    let core_receiver = GameCoreOutputReceiver {
+        receiver,
+        assets_server,
+    };
+
+    (core_actor, core_receiver) */
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct GameCoreActor<S: MessegeSender, A: AssetsServer> {
+    assets_server: A,
+    sender: S,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<S: MessegeSender, A: AssetsServer> GameCoreActor<S, A> {
+    pub fn send(&mut self, command: GameCoreActorCommand) {}
+}
+
+//--------------------------------------------------//
+//                                                  //
+//                  ASSETS SERVER                   //
+//                                                  //
+//--------------------------------------------------//
+/// 関数郡のようなもの
+/// 実態は軽量でコピー可能であるべき
+pub trait AssetsServer: Copy {}
+
+//--------------------------------------------------//
+//                                                  //
+//                      OUTPUT                      //
+//                                                  //
+//--------------------------------------------------//
+pub enum Output {
+    AnimatableFrames(AnimatableFrames),
+    SameTime(Vec<OutputFrame>),
+    GoNextWave,
+    Win,
+    Lose,
+}
+
+pub struct AnimatableFrames {
+    animation_id: AnimationId,
+    frames: Vec<OutputFrame>,
+}
+
+pub struct OutputFrame {
+    // 副作用が存在するが、作用自体は描画できない内部的な効果である場合、None
+    main_effect: Option<OutputEffect>,
+    sub_effects: Vec<OutputEffect>,
+}
+impl OutputFrame {
+    pub fn main_effect(&self) -> Option<&OutputEffect> {
+        self.main_effect.as_ref()
     }
+    pub fn sub_effects(&self) -> &Vec<OutputEffect> {
+        &self.sub_effects
+    }
+}
+impl TryFrom<&Frame> for OutputFrame {
+    // 特に伝えることはない
+    // Optionでいいならそうする
+    type Error = ();
+    fn try_from(value: &Frame) -> Result<Self, Self::Error> {
+        let main_effect = OutputEffect::try_from(&value.main_effect).ok();
+        let sub_effects = value
+            .sub_effects
+            .iter()
+            .filter_map(|e| OutputEffect::try_from(e).ok())
+            .collect::<Vec<_>>();
+
+        if main_effect.is_some() || !sub_effects.is_empty() {
+            Ok(Self {
+                main_effect,
+                sub_effects,
+            })
+        } else {
+            Err(())
+        }
+    }
+}
+
+pub enum OutputEffect {
+    Damage,
+    Heal,
+}
+
+impl TryFrom<&UpdateStateMessage> for OutputEffect {
+    type Error = ();
+    fn try_from(value: &UpdateStateMessage) -> Result<Self, Self::Error> {
+        match value {
+            UpdateStateMessage::Damage => Ok(Self::Damage),
+            _ => Err(()),
+        }
+    }
+}
+//--------------------------------------------------//
+//                                                  //
+//                     MESSAGE                      //
+//                                                  //
+//--------------------------------------------------//
+#[derive(Debug, Clone)]
+pub struct Message {
+    inner: PrivateMessage,
+}
+
+#[derive(Debug, Clone)]
+enum PrivateMessage {
+    SkillBegin(SkillId),
+    SkillEnd,
+    SameTimeBegin,
+    SameTimeEnd,
+    Frame(Frame),
+}
+
+impl PrivateMessage {
+    pub(crate) fn is_begin(&self) -> bool {
+        matches!(
+            self,
+            PrivateMessage::SameTimeBegin | PrivateMessage::SkillBegin(_)
+        )
+    }
+
+    pub(crate) fn is_end(&self) -> bool {
+        matches!(self, Self::SameTimeEnd | Self::SkillEnd)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Frame {
+    main_effect: UpdateStateMessage,
+    sub_effects: Vec<UpdateStateMessage>,
+}
+
+#[derive(Debug, Clone)]
+enum UpdateStateMessage {
+    Damage,
+    HealMp,
+}
+
+//--------------------------------------------------//
+//                                                  //
+//                      STATE                       //
+//                                                  //
+//--------------------------------------------------//
+
+pub enum WinOrLoseOrNextwave {
+    Win,
+    Lose,
+    Nextwave,
+}
+
+pub struct GameState {}
+
+impl GameState {
+    pub(crate) fn update(&mut self, message: &UpdateStateMessage) -> Option<WinOrLoseOrNextwave> {
+        todo!()
+    }
+}
+
+//--------------------------------------------------//
+//                                                  //
+//             MESSAGE SENDER RECEIVER              //
+//                                                  //
+//--------------------------------------------------//
+pub trait MessegeSender {
+    fn send(&mut self, output: Message) -> Result<(), Box<dyn std::error::Error>>;
+}
+pub trait MessageReceiver {
+    fn unblock_recv(&mut self) -> Result<Option<Message>, Box<dyn std::error::Error>>;
 }
